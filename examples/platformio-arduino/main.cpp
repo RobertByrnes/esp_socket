cad3405226fcb035d1e8b50d9a933d2694c76560
RobertByrnes <robbyrnes@hotmail.co.uk>
2023-03-12 22:07:32 +0000

    chore: Update example

--- examples/platformio-arduino/main.cpp
+++ examples/platformio-arduino/main.cpp
@@ -1,3 +1,5 @@
+// #define DEBUG_ESP_SOCKET
+
 #include <Arduino.h>
 #include <WiFi.h>
 #include <FS.h>
@@ -12,9 +14,55 @@
 TCallBattery battery;
 AsyncWebServer server(80);
 
+std::string ESP_REBOOT = "reboot";
+std::string ESP_UPDATE = "update";
+std::string ESP_PROVISION = "provision";
 
-void setup()
-{
+bool restart = false;
+
+void update() {
+  // implement wifi update function
+}
+
+void provision() {
+  // implement provisioning function
+}
+
+void onMsgHandler(uint8_t *data, size_t len) {
+  ESPSocket.print("[");
+  ESPSocket.print((int)millis());
+  ESPSocket.print("] ");
+
+  std::string action = "";
+  for (int i = 0; i < len; i++) {
+    action += char(data[i]);
+  }
+
+  // ESPSocket.println(action.c_str());
+
+  if (action == ESP_REBOOT) {
+    ESPSocket.println("Rebooting in 5 seconds, this page will reload...");
+    ESPSocket.println("[:RELOAD:]");
+    restart = true;
+    return;
+  }
+
+  if (action == ESP_UPDATE) {
+    ESPSocket.println("Processing update signal...");
+    update();
+    return;
+  }
+
+  if (action == ESP_PROVISION) {
+    ESPSocket.println("Provisioning...");
+    provision();
+    return;
+  }
+
+  ESPSocket.println("Invalid message...");
+}
+
+void setup() {
   Serial.begin(115200);
   Serial.printf("");
   WiFi.mode(WIFI_STA);
@@ -24,20 +72,25 @@ void setup()
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
-  
+
   Serial.println(WiFi.localIP());
 
-  ESPSocket.begin(&server, DEFAULT_CALLBACK);
-  
+  ESPSocket.msgCallback(onMsgHandler);
+  ESPSocket.begin(&server);
+
   server.begin();
-  
+
   Serial.println("ESP Socket OK");
 }
 
-void loop()
-{
+void loop() {
   ESPSocket.printBatteryInfo(battery);
   ESPSocket.printWiFiInfo();
   ESPSocket.printESPInfo();
+  ESPSocket.printf("Runtime: %u", millis());
   delay(DELAY_TIME);
+
+  if (restart == true) {
+    ESP.restart();
+  }
 }
